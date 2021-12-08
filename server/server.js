09692b3caaa7c3cc28a94ad573b36848c5104973
Eddie <emp8991@gmail.com>
2021-12-08 15:48:22 +0000

    Added a data.js file with all the results, made the server work and added the secondary button on index.html

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const server = express();
+const cors =require('cors');
 server.use(cors());
-
 //Initialise the port
 const port = 3000;
-
-
-    
-
+//Import the data
+const data = require('./data');
+const searchResults = data.searchResults;
+const resultsInformation = data.resultsInformation;
+//Show the list of possible results
+server.get('/', (req,res) =>{
+    res.send(resultsInformation);
+});
+//Show facts about a random result
+server.get('/random',(req,res) => {
+    res.send(resultsInformation[Math.floor(Math.random()*searchResults.length)]);
+});
 
 //Run server
 server.listen(port, () => {
